d8342890ab870cb4ca8270347a47f341e9a7d5fe
LewNic1 <nlewis365@gmail.com>
2019-04-03 14:39:24 -0700

    modified app; layout and signin templates

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
-from flask import Flask, g 
-from flask import render_template, flash, redirect, url_for
-from flask_login import LoginManager, login_user, login_required, current_user
+from flask import Flask, g, request
+from flask import render_template, flash, redirect, url_for, session, escape 
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 from flask_bcrypt import check_password_hash
+from werkzeug.urls import url_parse 
 
 # import json 
 import models
 import forms 
+import os 
 
 DEBUG = True
 PORT = 8000
 
-app = Flask(__name__) 
+app = Flask(__name__, instance_relative_config=True) 
+app.config.from_pyfile('flask.ctg')
 app.secret_key = 'molokai'
 
 
 login_manager = LoginManager() #sets up login
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+# images = UploadSet('imges', IMAGES)
+# configure_uploads(app, images)
 
 @login_manager.user_loader
 def load_user(userid):
@@ -30,6 +36,7 @@ def load_user(userid):
 def before_request():
     g.db = models.DATABASE   #CONNECTS TO DB BEFORE EACH REQUEST
     g.db.connect()
+    g.user = current_user 
 
 @app.after_request
 def after_request(response):   #CLOSES THE DB AFTER EACH REQUEST
@@ -46,18 +53,26 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/signup', methods=('GET', 'POST'))
-def SignUpForm():
+@app.route('/signup', methods=['GET', 'POST'])
+def SignUp():
     form = forms.SignUpForm()
     if form.validate_on_submit():
-        flash('Sign Up Success!')
+        # filename = images.save(request.files['profile_image'])
+        # url = images.url(filename)
+       
         models.User.create_user(
             username=form.username.data,
             email=form.email.data,
             password=form.password.data 
+            location=form.location.data
+            # image_filename=filename,
+            # image_url=url
             )
-        
-        return redirect(url_for('index'))
+        user = models.User.get(models.User.username == form.username.data)
+        login_user(user)
+        name = user.username
+        flash('Sign Up Success!') 
+        return redirect(url_for('profile', username=name))
     return render_template('signup.html', form=form) 
 
 @app.route('/signin', methods=('GET', 'POST'))
@@ -77,6 +92,12 @@ def signin():
                 flash("Error", "Passeord or email is incorrect")
     return render_template('signin.html', form=form) 
 
+@app.route('/signout')
+@login_required
+def signout():
+    signout_user()
+    flash("Sign out success!")
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
